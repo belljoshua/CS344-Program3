2e11076bd359aaea1e1e659c564cc06dd6c6b912
Joshua Bell <belljos@oregonstate.edu>
2018-03-02 23:24:10 -0800

    Program3

--- Program3.c
+++ Program3.c
@@ -12,6 +12,8 @@ int main(){
 	
 	while(1){
 		int i = 0, j = 0;		//Utility vars for incrementing
+		int sourceFD, targetFD; //File descriptors
+		char* args[512];		//Arguments to be passed to execvp
 
 		//Get user input
 		char* inptBuffer = NULL;
@@ -38,15 +40,14 @@ int main(){
 				for(j = strlen(inptBuffer)-1; j > i+1; j--){
 					inptBuffer[j+PIDlen-2] = inptBuffer[j];
 				}
+				//Insert PID
 				for(j = 0; j < PIDlen; j++){
 					inptBuffer[i+j] = myPIDstr[j];
 				}
 			}
 		}
 
-		printf("inptBuffer: %s\n", inptBuffer);
-
-		char* inptArgs[512];
+		char* inptArgs[512];		//Array of cmd line input
 		for(i = 0; i < 512; i++){
 			inptArgs[i] = "\0";
 		}
@@ -61,28 +62,31 @@ int main(){
 		}
 		numInptArgs = i;
 
-		for(i = 0; i < numInptArgs; i++){
-			printf("%s ", inptArgs[i]);
-		}
-		printf("\n");
-
 		//Exit
 		if(!strcmp("exit", inptArgs[0])){
-			printf("Exiting..\n");
+			//printf("Exiting..\n");
 			exit(0);
 		}
 
 		//cd
-		else if(!strcmp("cd", inptArgs[0])){
+		char* homeDir = getenv("HOME");
+		if(!strcmp("cd", inptArgs[0])){
+			//If no argument given to cd
+			if(!strcmp("\0", inptArgs[1])){
+				printf("Changing to %s\n", homeDir);
+				chdir(homeDir);
+			}
+			//Argument given to cd
+			else{
 			chdir(inptArgs[1]);
+			}
 		}
 
 		//Status
 		else if(!strcmp("status", inptArgs[0])){
-			
+
 		}
 		else if(inptArgs[0][0] != '#'){
-			printf("Not a comment..\n");
 
 			spawnpid = fork();
 			switch (spawnpid){
@@ -92,19 +96,58 @@ int main(){
 					exit(1);
 					break;
 				}
+				//Child
 				case 0:{
-					printf("Running your command..\n"); fflush(stdout);
-					execlp(inptArgs[0], inptArgs[0], NULL);
+
+					j = 0;		//Incrementor for args
+
+					for(i = 0; i < numInptArgs; i++){
+
+						//Redirect stdin
+						if(inptArgs[i][0] == '<'){
+							printf("Pointing stdin to %s\n", inptArgs[i+1]);
+							sourceFD = open(inptArgs[i+1], O_RDONLY);
+							if(sourceFD == -1){
+								printf("Failed to open file %s\n", inptArgs[i+1]);
+								perror("error opening file");
+								exit(1);
+							}
+							fcntl(sourceFD, F_SETFD, FD_CLOEXEC);
+							dup2(sourceFD, 0);
+						}
+						//Redirect stdout
+						else if(inptArgs[i][0] == '>'){
+							printf("Pointing stdout to %s\n", inptArgs[i+1]);
+							targetFD = open(inptArgs[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+
+							//Error if failed to open
+							if(targetFD == -1){
+								perror("error opening file");
+								exit(1);
+							}
+							else{
+								fcntl(targetFD, F_SETFD, FD_CLOEXEC);
+								dup2(targetFD, 1);
+							}
+						}
+						//Add arguments for exec
+						else if((i != 0 && inptArgs[i-1][0] != '>' && inptArgs[i-1][0] != '<') || i == 0){
+							args[j] = inptArgs[i];	
+							j++;
+						}
+					}
+					args[j] = NULL;
+					execvp(args[0], args);
 					exit(0);
 					break;
 				}
+				//Parent
 				default:{
 					pid_t actualPid = waitpid(spawnpid, &childExitStatus, 0);
-					printf("I am the parent!\n"); fflush(stdout);
+					//printf("I am the parent!\n"); fflush(stdout);
 					break;
 				}
 			}
-			printf("this will be executed by both of us!\n");
 		}
 	}
 }
